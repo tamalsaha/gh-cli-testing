897da08dd631b19da5a431fe2a335a9db0bb5054
Tamal Saha <tamal@appscode.com>
2020-06-06 11:25:18 -0700

    gh-cli-testing
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- update.sh
+++ update.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+git commit -a -s -m "gh-cli-testing"
+hub pull-request \
+    --push \
+    --labels automerge \
+    --message "$COMMIT_MSG" \
+    --message "Signed-off-by: $(git config --get user.name) <$(git config --get user.email)>"
